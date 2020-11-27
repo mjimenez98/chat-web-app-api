7203fcf55559ce073567a619e93f463d445f237d
dinasadir <45443818+dinasadir@users.noreply.github.com>
2020-11-27 15:03:06 -0500

    Working version

--- src/main/java/chat/ChatManager.java
+++ src/main/java/chat/ChatManager.java
@@ -25,4 +25,51 @@ public class ChatManager implements Serializable {
 
         return newMessage;
     }
+
+    public LinkedList<Message> listMessages(LocalDateTime startDate, LocalDateTime endDate) {
+        if (chat == null)
+            chat = new LinkedList<>();
+
+        Stream<Message> messagesToKeepStream = chat.stream();
+
+        if (startDate != null && endDate != null) {
+            messagesToKeepStream = chat.stream().filter(postedMessage ->
+                    postedMessage.getTimestamp().isAfter(startDate) & postedMessage.getTimestamp().isBefore(endDate));
+        } else if (startDate != null) {
+            messagesToKeepStream = chat.stream().filter(postedMessage ->
+                    postedMessage.getTimestamp().isAfter(startDate));
+        } else if (endDate != null) {
+            messagesToKeepStream = chat.stream().filter(postedMessage ->
+                    postedMessage.getTimestamp().isBefore(endDate));
+        }
+
+        return messagesToKeepStream.collect(Collectors.toCollection(LinkedList::new));
+    }
+
+    public LinkedList<Message> getChat() {
+        return chat;
+    }
+
+    public void clearChat(LocalDateTime startDate, LocalDateTime endDate) {
+        if (startDate == null && endDate == null) {
+            chat.clear();
+        } else {
+            Stream<Message> messagesToKeepStream = chat.stream();
+
+            if (startDate != null && endDate != null) {
+                messagesToKeepStream = chat.stream().filter(postedMessage ->
+                        postedMessage.getTimestamp().isBefore(startDate) || postedMessage.getTimestamp().isAfter(endDate));
+            } else if (startDate != null) {
+                messagesToKeepStream = chat.stream().filter(postedMessage ->
+                        postedMessage.getTimestamp().isBefore(startDate));
+            } else if (endDate != null) {
+                messagesToKeepStream = chat.stream().filter(postedMessage ->
+                        postedMessage.getTimestamp().isAfter(endDate));
+            }
+
+            LinkedList<Message> filteredChat = new LinkedList<>();
+            messagesToKeepStream.forEach(filteredChat::add);
+            chat = filteredChat;
+        }
+    }
 }
--- src/main/java/chat/ChatServer.java
+++ src/main/java/chat/ChatServer.java
@@ -6,6 +6,8 @@ import io.grpc.stub.StreamObserver;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.LinkedList;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
@@ -69,9 +71,10 @@ public class ChatServer {
      */
     private static class ChatManagerImpl extends ChatManagerGrpc.ChatManagerImplBase {
         private ChatManager chatManager;
+        static DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
 
-        ChatManagerImpl(){
-           chatManager = new ChatManager();
+        ChatManagerImpl() {
+            chatManager = new ChatManager();
         }
 
         @Override
@@ -80,13 +83,46 @@ public class ChatServer {
             String message = request.getMessage();
             String user = request.getUser();
             Message newMessage = chatManager.postMessage(message, user);
-            if(newMessage != null){
-                responseObserver.onNext(chat.Response.newBuilder().setResponse("SUCCESS").build());
-            }
-            else{
-                responseObserver.onNext(chat.Response.newBuilder().setResponse("FAILURE").build());
+            if (newMessage != null) {
+                responseObserver.onNext(chat.Response.newBuilder().setResponse("200: OK").build());
+            } else {
+                responseObserver.onNext(chat.Response.newBuilder().setResponse("400: ERROR").build());
             }
             responseObserver.onCompleted();
         }
+
+        @Override
+        public void listMessages(Request request, StreamObserver<Msg> responseObserver) {
+            LocalDateTime start = getLDT(request.getStartDate());
+            LocalDateTime end = getLDT(request.getEndDate());
+            ;
+            LinkedList<Message> chat = chatManager.listMessages(start, end);
+            for (Message message : chat) {
+                Msg.Builder msg = Msg.newBuilder();
+                msg.setUser(message.getUser());
+                msg.setMessage(message.getMessage());
+                msg.setDate(message.getTimestamp().format(formatter));
+                responseObserver.onNext(msg.build());
+            }
+            responseObserver.onCompleted();
+        }
+
+        @Override
+        public void clearChat(chat.Request request,
+                              io.grpc.stub.StreamObserver<chat.Response> responseObserver) {
+
+            LocalDateTime start = getLDT(request.getStartDate());
+            LocalDateTime end = getLDT(request.getEndDate());
+            chatManager.clearChat(start, end);
+            responseObserver.onNext(chat.Response.newBuilder().setResponse("CHAT CLEARED!").build());
+            responseObserver.onCompleted();
+        }
+    }
+
+    private static LocalDateTime getLDT(String date) {
+        if (date != null && date.length() > 0) {
+            return LocalDateTime.parse(date);
+        }
+        return null;
     }
 }
